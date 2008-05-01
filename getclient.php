505dcd202ee510131bd0b99bf73b3fb8f7c1469f
nugget <nugget>
2008-05-01 13:38:39 +0000

    08:29 <dp> just curious, since getclient.php is missing from the tarball :)

---
+++ getclient.php
@@ -0,0 +1,29 @@
+<?php
+
+  include "init.inc";
+
+  import_request_variables("cpg","rvar_");
+
+  header('Cache-Control: max-age=0, must-revalidate, no-cache, no-store');
+  header('Content-Type: text/plain');
+
+  $user = sanitize_word($rvar_user);
+  $client_script = "./client/netskel";
+
+  if(!is_dir("$root_dir$user")) {
+    header('HTTP/1.0 404 Not Found');
+    die;
+  }
+
+  $buf = custom_client_script($user);
+
+  header('Content-type: text/plain');
+#  header('Content-Disposition: attachment; filename=netskel');
+#  header('Content-Description: netskel client script');
+  header('Cache-Control: no-store, no-cache, must-revalidate');
+  header('Cache-Control: post-check=0, pre-check=0', false);
+  header('Pragma: no-cache');
+
+  print $buf;
+
+?>
